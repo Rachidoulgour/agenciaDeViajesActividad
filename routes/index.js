063d5c2940113d0a37e086518e205a4a5451cf4b
rachid <olgor@hotmail.com>
2019-06-02 17:39:51 +0200

    Casi terminada la actividad1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Agencia Viajes' });
 });
+router.get('/', function(req, res, next) {
+  res.render('layout', { title:'Agencia layout'});
+})
 
 module.exports = router;
